6a695b76ee91fae28a2f8da00312cfa826381119
SladjanaColakovic <colakovic.ra32.2018@uns.ac.rs>
2023-10-19 23:28:27 +0200

    report front

--- time-sheet-front/src/reports/Search.js
+++ time-sheet-front/src/reports/Search.js
@@ -1,21 +1,129 @@
-const Search = () => {
+import { useEffect, useState } from "react";
+import { getRequest, getRequestWithParams } from "../requests/httpClient";
+import axiosInstance from "../requests/axiosIntance";
+
+const Search = ({ setData }) => {
+
+    const [clients, setClients] = useState();
+    const [projects, setProjects] = useState();
+    const [categories, setCategories] = useState();
+    const [teamMembers, setTeamMembers] = useState();
+    const [client, setClient] = useState()
+    const [project, setProject] = useState()
+    const [teamMember, setTeamMember] = useState()
+    const [category, setCategory] = useState()
+    const [startDate, setStartDate] = useState("")
+    const [endDate, setEndDate] = useState("");
+
+    useEffect(() => {
+        getRequest("http://localhost:8080/api/client")
+            .then((res) => {
+                console.log(res.data.clients)
+                setClients(res.data.clients);
+                getRequest("http://localhost:8080/api/project")
+                    .then((res) => {
+                        setProjects(res.data.projects);
+                        getRequest("http://localhost:8080/api/teamMember")
+                            .then((res) => {
+                                setTeamMembers(res.data.teamMembers);
+                                getRequest("http://localhost:8080/api/category")
+                                    .then((res) => {
+                                        setCategories(res.data.categories)
+                                    })
+                            })
+                    })
+            })
+    }, [])
+
+
+    const handleSearch = () => {
+        let data = {
+            clientId: client,
+            categoryId: category,
+            teamMemberId: teamMember,
+            projectId: project,
+            startDate: startDate,
+            endDate: endDate
+        }
+        
+    //     const params = { clientId: client }
+    //    axiosInstance.get("http://localhost:8080/api/report", {params}).
+    //    then((res) => {
+    //     console.log(res)
+    //    })
+    }
+
     return (
         <div>
-            
             <div className="row">
                 <div className="col-1"></div>
                 <div className="col-10">
-                    <div className="row">
-                        <div className="col-4">
-                            <label>Team member</label>
-                            <select>
-                                <option value=""></option>
-                            </select>
+                    <div className="search">
+                        <div className="row">
+                            <div className="col-4">
+                                <label>Team member</label>
+                                <select onChange={(e) => { setTeamMember(e.target.value) }}>
+                                    <option>All</option>
+                                    {teamMembers && teamMembers.map((teamMember) => (
+                                        <option key={teamMember.id} value={teamMember.id}>{teamMember.name}</option>
+                                    ))}
+
+                                </select>
+                            </div>
+                            <div className="col-4">
+                                <label>Client</label>
+                                <select onChange={(e) => { setClient(e.target.value) }}>
+                                    <option>All</option>
+                                    {clients && clients.map((client) => (
+                                        <option key={client.id} value={client.id}>{client.name}</option>
+                                    ))}
+
+                                </select>
+                            </div>
+                            <div className="col-4">
+                                <label>Project</label>
+                                <select onChange={(e) => { setProject(e.target.value) }}>
+                                    <option>All</option>
+                                    {projects && projects.map((project) => (
+                                        <option key={project.id} value={project.id}>{project.name}</option>
+                                    ))}
+
+                                </select>
+                            </div>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <div className="col-4">
+                                <label>Category</label>
+                                <select onChange={(e) => { setCategory(e.target.value) }}>
+                                    <option value="">All</option>
+                                    {categories && categories.map((category) => (
+                                        <option key={category.id} value={category.id}>{category.name}</option>
+                                    ))}
+
+                                </select>
+                            </div>
+                            <div className="col-4">
+                                <label>Start date</label>
+                                <input type="date" value={startDate} onChange={(e) => { setStartDate(e.target.value) }} />
+                            </div>
+                            <div className="col-4">
+                                <label>End date</label>
+                                <input type="date" value={endDate} onChange={(e) => { setEndDate(e.target.value) }} />
+                            </div>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <div className="col-10"></div>
+                            <div className="col-2">
+                                <button onClick={handleSearch} className="search-btn">Search</button>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-1"></div>
             </div>
+            <br />
         </div>
     );
 }
